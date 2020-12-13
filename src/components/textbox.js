1407462cb8b24e9f8017f6ddabf516ef8e5c0170
rrkane <raj.r.kane@protonmail.com>
2020-12-12 18:05:18 -0700

    output styling

--- src/components/textbox.js
+++ src/components/textbox.js
@@ -1,14 +1,26 @@
-import React from "react"
+import React, {useState} from "react"
 import styles from "./textbox.module.css"
 
-const Textbox = () => (
-    <div style={{background: "#f4f4f4"}}>
-        <form className={styles.part}>
-            <textarea id={styles.input} placeholder="Enter names here..." autofocus></textarea>
-            <button type="submit" id={styles.generate} >Generate your pairings</button>
-        </form>
-        <p>Outputs</p>
-    </div>
-)
+const Textbox = () => {
+
+    const [showOutput, toggleShowOutput] = useState(false)
+
+    const onGenerate = (e) => {
+        e.preventDefault()
+        toggleShowOutput(!showOutput) 
+    }
+
+    return (
+        <div style={{background: "#f4f4f4"}}>
+            <form className={styles.part}>
+                <textarea id={styles.input} className={styles.box} placeholder="Enter names here..." autofocus></textarea>
+                <button id={styles.generate} onClick={e => onGenerate(e)} >Generate your pairings</button>
+            </form>
+            {showOutput === true && 
+                <textarea readOnly id={styles.output} className={styles.box}>Outputs</textarea>
+            }
+        </div>
+    )
+}
 
 export default Textbox
