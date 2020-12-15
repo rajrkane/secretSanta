601ab4b8a3d9a95919d73e760030ea66fe94bb41
rrkane <raj.r.kane@protonmail.com>
2020-12-14 21:49:45 -0700

    initial link setup

--- src/components/secretSanta.js
+++ src/components/secretSanta.js
@@ -6,10 +6,25 @@ const SecretSanta = ({ input }) => {
     const names = input.split(',')
     names.forEach(name => name.trim())
     let pairs = {}
+    let links = []
 
     const makePairs = () => {
         names.forEach(santa => assignRecipient(santa))
-        return pairs
+        // return pairs
+        return links
+    }
+
+    // const writeToFile = (santa) => {
+    //     fs.writeFile(`${santa}SecretSanta.txt`, `Hi ${santa}, your pair is ${pairs[santa]}`)
+    // }
+
+    const setDownloadableLink = (santa) => {
+        const element = document.createElement('a') // create anchor element
+        console.log('here')
+        element.setAttribute("href", 'data:text/plain;charset=utf-8,' + encodeURIComponent(`Hi ${santa}, your pair is ${pairs[santa]}`))
+        element.setAttribute("download", `${santa}SecretSanta.txt`)
+        links.push(element)
+        console.log('links ', links)
     }
 
     const assignRecipient = (santa) => {
@@ -17,10 +32,11 @@ const SecretSanta = ({ input }) => {
                                     .filter(name => !Object.values(pairs).includes(name)) // no duplicate recipients
                                     .filter(name => pairs[name] != santa) // no 2-way match loops
         pairs[santa] = possibleRecipients[Math.floor(Math.random() * possibleRecipients.length)]
+        setDownloadableLink(santa)
     }
 
     return (
-        <textarea readOnly id={styles.output} className={styles.box} value={JSON.stringify(makePairs())}/>
+        <textarea readOnly id={styles.output} className={styles.box} value={makePairs()}/>
     )
 }
 
